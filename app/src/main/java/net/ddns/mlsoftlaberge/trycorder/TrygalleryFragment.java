ece44283380353bb1ffcce9af130595ca5e51846
Martin Laberge <mlsoftlaberge@gmail.com>
2017-02-06 00:11:02 -0500

    version 5.1.0 added the apache license in every source files

--- app/src/main/java/net/ddns/mlsoftlaberge/trycorder/TrygalleryFragment.java
+++ app/src/main/java/net/ddns/mlsoftlaberge/trycorder/TrygalleryFragment.java
@@ -1,5 +1,27 @@
 package net.ddns.mlsoftlaberge.trycorder;
 
+/*
+*  By Martin Laberge (mlsoft), From March 2016 to november 2016.
+*  Licence: Can be shared with anyone, for non profit, provided my name stays in the comments.
+*  This is a conglomerate of examples codes found in differents public forums on internet.
+*  I just used the public knowledge to fit a special way to use an android phone functions.
+*/
+
+/* Copyright 2016 Martin Laberge
+*
+*        Licensed under the Apache License, Version 2.0 (the "License");
+*        you may not use this file except in compliance with the License.
+*        You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*        Unless required by applicable law or agreed to in writing, software
+*        distributed under the License is distributed on an "AS IS" BASIS,
+*        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*        See the License for the specific language governing permissions and
+*        limitations under the License.
+*/
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
